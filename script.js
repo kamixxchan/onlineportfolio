a5d53857ebfeac655a88810949636ad8b206c959
kamixxchan <pauchokvalera@gmail.com>
2023-11-24 14:22:42 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const themeToggle = document.querySelector('[theme-toggle]');
+//     const body = document.body;
+
+//     themeToggle.addEventListener("click", function () {
+//         if (body.classList.contains("light")) {
+//             body.classList.replace("light", "dark");
+//         } else {
+//             body.classList.replace("dark", "light");
+//         }
+//     });
+// });
+
+
+// /**
+//  * 
+//  * 
+//  * @param {number} currentHour
+//  * @returns {string} 
+//  */
+// const getGreetingMsg = function(currentHour){
+//     const greeting = 
+//     currentHour < 5 ? 'Night' : 
+//     currentHour < 12 ? 'Morning':
+//     currentHour < 15 ? 'Noon' :
+//     currentHour < 17 ? 'Afternoon':
+//     currentHour < 20 ? 'Evening' : 
+//     'Night';
+
+//     return `Good ${greeting}`;
+// }
+
+// const $greetElem = document.querySelector('[data-greeting]');
+// const currentHour = new Date().getHours();
+// $greetElem.textContent = getGreetingMsg(currentHour);
+
+
+
+
+
+
